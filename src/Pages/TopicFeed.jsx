56d123429ddeb89fad260ff9701e57e419c0a4de
Joao Silva <joaodoer3@gmail.com>
2024-08-03 19:33:55 -0300

    bibliotecas adicionadas

--- src/Pages/TopicFeed.jsx
+++ src/Pages/TopicFeed.jsx
@@ -1,7 +1,5 @@
 import { Avatar, Card, CardContent, CardHeader, Container, IconButton, Typography } from "@mui/material";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { useState } from "react";
+
 import CardFeed from "../Components/CardFeed";
 
 const TopicFeed = () => {
